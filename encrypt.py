e46165f6de69870c4c7331cc1f9c2435722add6c
Masroor Khonkhodzhaev <masroor47@icloud.com>
2020-08-15 22:52:32 -0400

    Simplified char encoding and encryption

--- encrypt.py
+++ encrypt.py
@@ -1,20 +1,15 @@
 def encrypt(e, N, message):
     ascii_values = []
-    
-    # Placing the ASCII values of the string characters in an Int array
+
     for i in message:
-        ascii_values.append(ord(i))
+        ascii_values.append(pow (ord(i), e) % N)
+    print(f"Length: {len(ascii_values)}")
 
-    print(ascii_values)
-
-    # Replacing each value with the encrypted version of itself
-    for i in range(len(ascii_values)):
-        ascii_values[i] = pow (ascii_values[i], e) % N
-        
     return ascii_values
 
+# Encrypts each char as a sseparate int in an array
 message_str = input("Enter the message: ")
 e = int(input("Enter the encryption key: "))
 N = int(input("Enter the N: "))
 
-print(f"The encrypted message is: {encrypt(e, N, message_str)}")
+print(f"The encrypted message is: \n{encrypt(e, N, message_str)}")
