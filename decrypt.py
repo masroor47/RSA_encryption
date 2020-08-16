c098e959e061711d80bf877bc12a6f5577af5040
Masroor Khonkhodzhaev <masroor47@icloud.com>
2020-08-15 22:53:39 -0400

    Now it takes an array of encrypted integers and returns a decrypted string

--- decrypt.py
+++ decrypt.py
@@ -1,8 +1,23 @@
-def decrypt(d, N, enc_message):
-    return pow(enc_message, d) % N
+def decrypt(d, N, ascii_values):
+    message = ""
+    for i in range(len(ascii_values)):
+        ascii_values[i] = pow (ascii_values[i], d) % N
 
-to_decrypt = int(input("What do you want to decrypt: "))
+    print(ascii_values)
+    for i in ascii_values:
+        message += chr(i)
+    return message
+
+# Decrypts each value as an individual char
+ascii_values = []
+value = int(input("Enter value: "))
+while value != -1:
+    ascii_values.append(value)
+    value = int(input("Enter value: "))
+
+print("You entered: " + ascii_values)
+
 d = int(input("Enter the private key: "))
 N = int(input("Enter the N: "))
 
-print(f"The decrypted message is: {decrypt(d, N, to_decrypt)}")
+print(f"The decrypted message is: {decrypt(d, N, ascii_values)}")
